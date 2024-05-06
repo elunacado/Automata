d6ac7d12c17dc0ba89537292c8abf7be95e08340
TuLocalHost <54047744+elunacado@users.noreply.github.com>
2024-05-06 08:47:57 -0600

    Update automata.pl
    I updated the automata, to increase its efficiency

--- automata.pl
+++ automata.pl
@@ -1,63 +1,60 @@
 %estado inicial
-estado_inicial(start).
+estado_inicial(q1).
 %--------------------
 
 %estados de exito
-estado_exito(q3). %ilm
-estado_exito(q12). %ikhut-eigh
-estado_exito(q17). %ichwan
-estado_exito(q21). %idras
-estado_exito(q22). %idray
+estado_exito(q19). %ilm
+
 %--------------------
 
 % movernos a traes del automata
 % move(from, with, to)
 
 %iniciamos con i en todas
-move(start, i, q1).
+move(q1, i, q2).
 
 %caso de ilm
-move(q1, l, q2).
-move(q2, m, q3).
+move(q2, l, q7).
+move(q7, m, q19).
 
 %caso de ikhut-eigh
-move(q1, k ,q4).
-move(q4, h ,q5).
-move(q5, u ,q6).
-move(q6, t ,q7).
-move(q7, - ,q8).
-move(q8, e ,q9).
-move(q9, i ,q10).
-move(q10, g ,q11).
+move(q2, k ,q11).
 move(q11, h ,q12).
+move(q12, u ,q13).
+move(q13, t ,q14).
+move(q14, - ,q15).
+move(q15, e ,q16).
+move(q16, i ,q17).
+move(q17, g ,q18).
+move(q18, h ,q19).
 
 %caso de ichwan
-move(q1, c, q13).
-move(q13, h, q14).
-move(q14, w, q15).
-move(q15, a, q16).
-move(q16, n, q17).
+move(q2, c, q3).
+move(q3, h, q4).
+move(q4, w, q5).
+move(q5, a, q6).
+move(q6, n, q19).
 
 %Caso de idray e idras
-move(q1, d, q18).
-move(q18, r, q19).
-move(q19, a, q20).
+move(q2, d, q8).
+move(q8, r, q9).
+move(q9, a, q10).
 
 %cambio a idras
-move(q20, s, q21).
+move(q10, s, q19).
 
 %cambio a idray
-move(q20, y, q22).
+move(q10, y, q19).
 
 % codigo de aceptación()
 %recibe un string entre comillas
 aceptar(String) :-
     %separo el string por caracteres
     string_chars(String, ListaCaracteres),
-    %llamo al estado inicial start
-    estado_inicial(start),
+    %llamo al estado inicial q1
+    estado_inicial(q1),
     %le paso a la funcion auxiliar la lista de caracteres y el estado inicial
-    aceptar_aux(ListaCaracteres, start).
+    aceptar_aux(ListaCaracteres, q1).
 
 aceptar_aux([], EstadoActual) :-
     %comprueba el estado inicial cuando se vacia la lista y nos da el resultado de true|false
@@ -70,19 +67,17 @@ aceptar_aux([Simbolo|Resto], EstadoActual) :-
     aceptar_aux(Resto, EstadoSiguiente).
 
 % Casos de prueba
-prueba_aceptada1 :- aceptar("ilm"), write('ACEPTADO: ilm'), nl.
-prueba_aceptada2 :- aceptar("ikhut-eigh"), write('ACEPTADO: ikhut-eigh'), nl.
-prueba_aceptada3 :- aceptar("ichwan"), write('ACEPTADO: ichwan'), nl.
-prueba_aceptada4 :- aceptar("idras"), write('ACEPTADO: idras'), nl.
-prueba_aceptada5 :- aceptar("idray"), write('ACEPTADO: idray'), nl.
-
-prueba_rechazada1 :- \+ aceptar("holaMundo"), write('RECHAZADO: holaMundo'), nl.
-prueba_rechazada2 :- \+ aceptar("illm"), write('RECHAZADO: illm'), nl.
-prueba_rechazada3 :- \+ aceptar("idra"), write('RECHAZADO: idra'), nl.
-prueba_rechazada4 :- \+ aceptar("ikhu"), write('RECHAZADO: ikhu'), nl.
-prueba_rechazada5 :- \+ aceptar("ichw"), write('RECHAZADO: ichw'), nl.
+prueba1 :- aceptar("ilm"), write('ACCEPTED: ilm'), nl.
+prueba2 :- aceptar("ikhut-eigh"), write('ACCEPTED: ikhut-eigh'), nl.
+prueba3 :- aceptar("ichwan"), write('ACCEPTED: ichwan'), nl.
+prueba4 :- aceptar("idras"), write('ACCEPTED: idras'), nl.
+prueba5 :- aceptar("idray"), write('ACCEPTED: idray'), nl.
+prueba6 :- \+ aceptar("holaMundo"), write('REJECTED: holaMundo'), nl.
+prueba7 :- \+ aceptar("illm"), write('REJECTED: illm'), nl.
+prueba8 :- \+ aceptar("idra"), write('REJECTED: idra'), nl.
+prueba9 :- \+ aceptar("ikhu"), write('REJECTED: ikhu'), nl.
+prueba10 :- \+ aceptar("ichw"), write('REJECTED: ichw'), nl.
 
 % Función para correr todos los casos de prueba
 runTests :-
-    prueba_aceptada1, prueba_aceptada2, prueba_aceptada3, prueba_aceptada4, prueba_aceptada5,  
-    prueba_rechazada1, prueba_rechazada2, prueba_rechazada3, prueba_rechazada4, prueba_rechazada5.
+    prueba1, prueba2, prueba3, prueba4, prueba5, prueba6, prueba7, prueba8, prueba9, prueba10.
